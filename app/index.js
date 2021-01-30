553f80c54593629ee8a66c5a241f9b8d27655d9f
MagonxESP <janma.360@gmail.com>
2021-01-30 21:54:30 +0100

    fix gallery overflow x

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import $ from 'jquery';
 
+function fixGalleryOverflow() {
+    let $contentWrapper = $('#content-wrapper');
+    let $content = $('#content');
+
+    $contentWrapper.toggleClass('is-gallery', $content.hasClass('app-gallery'));
+}
+
 $(() => {
 
     let favicon = '/custom/animu-theme/core/img/favicon.ico';
@@ -7,4 +14,10 @@ $(() => {
     $('link[rel="icon"]').attr('href', favicon);
     $('link[rel="apple-touch-icon-precomposed"]').attr('href', favicon);
 
+
+    $(document).on('click', '.view-switcher', () => {
+        fixGalleryOverflow();
+    });
+
+    fixGalleryOverflow();
 });
